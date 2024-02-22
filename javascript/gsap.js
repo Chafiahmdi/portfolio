3343faec61950dbf12346d3a8ebd4f4eeeb4aab7
Chafiahmdi <chabxl2022@icloud.com>
2024-02-22 23:17:29 +0100

    gsap home's title animation

--- javascript/gsap.js
+++ javascript/gsap.js
@@ -6,12 +6,12 @@ let tl = gsap.timeline();
                             opacity: 0,
                             x:-50 // Animation sur l'axe X
                         }); 
-                        tl.from(".intro h1", {     
-                            duration:1,
-                            opacity: 0,
-                            y: -50,  // Animation sur l'axe Y
-                            ease: "power2.Out" //Courbe d'accélération de l'animation
-                        }); 
+                        // tl.from(".intro h1", {     
+                        //     duration:1,
+                        //     opacity: 0,
+                        //     y: -50,  // Animation sur l'axe Y
+                        //     ease: "power2.Out" //Courbe d'accélération de l'animation
+                        // }); 
                         tl.from(".intro p", {     
                           duration:1,
                           opacity: 0,
@@ -21,11 +21,57 @@ let tl = gsap.timeline();
         
 
 })     
-                  gsap.to(".copyright", {     
-                            duration:1,
-                            y: -9,  // Animation sur l'axe Y
-                            scrollTrigger: ".copyright",
-                          })
+                  // gsap.to(".copyright", {     
+                  //           duration:1,
+                  //           y: -9,  // Animation sur l'axe Y
+                  //           scrollTrigger: ".copyright",
+                  //         })
+
+
+// ECRITURE ACCUEIL
+  /* --- Split the text, Great Horned Owl --- */
+  var tlSplitGreat = gsap.timeline({onComplete: () => {SplitGreat.revert()}}), 
+      SplitGreat = new SplitText(".titre", {type:"words,chars"}), 
+      chars = SplitGreat.chars;
+
+  // qd j le commente il fonctionne plus
+  tlSplitGreat.from(chars, {
+    duration: 0.8,
+    opacity:0,
+    y:10,
+    ease:"circ.out",
+    stagger: 0.02,
+  }, "+=0");
+
+  /* --- Split the text, Burrowing Owl --- */
+  function setupSplits() {
+    
+  var tlSplitBurrowing = gsap.timeline(), 
+      SplitBurrowing = new SplitText(".titleBurrowing", {type:"words,chars"}), 
+      chars = SplitBurrowing.chars; //an array of all the divs that wrap each character
+
+  tlSplitBurrowing.from(chars, {
+    duration: 0.8,
+    opacity:0,
+    y:10,
+    ease:"circ.out",
+    stagger: 0.02,
+  scrollTrigger: {
+      trigger: ".titleBurrowing",
+  //markers:true,
+      start: "top 75%",
+    end: "bottom center",
+      scrub:1
+    }
+  //,   onComplete: () => {SplitBurrowing.revert()}
+  }, "+=0");
+    
+  };
+//  ECRITURE ACCUEIL
+
+
+
+
 
 //   rotation: 360, // Rotation de l'image
                         // }); 
